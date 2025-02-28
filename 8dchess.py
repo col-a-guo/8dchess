42b153b4d54c3eb90bcb23cfee9bcd669fc4d142
Collin A Guo <bunjuku8@gmail.com>
2025-02-28 00:29:52 -0800

    bytes only now

--- 8dchess.py
+++ 8dchess.py
@@ -13,32 +13,28 @@ def pad_right_list(lst, length, value=None):
         return lst
     return lst + [value] * (length - len(lst))
 
-def create_hypercube_and_shift(dimension, total_cube_size, key_hex, bit_shift_size, cube_data):
+def create_hypercube_and_shift(dimension, total_cube_size, key_hex, cube_data):  #Removed bit_shift_size
+    cube_side_length = 2 ** (total_cube_size // dimension)
+    
     def pad_or_truncate_key(key_hex):
       key_temp = key_hex
-      size = int((2**(total_cube_size))*dimension*(2**bit_shift_size))
+      size = (2**total_cube_size) * dimension  # Each shift is now 1 byte
       if len(key_hex) < size:
-        key_temp = pad_right_list(key_hex, size, "0")
+        key_temp = pad_right_list(key_hex, size, 0) #Pad with 0 instead of "0" (integers)
       elif len(key_hex) > size:
         key_temp = key_hex[0:size]
       else:
         key_temp = key_hex
-      return(key_temp)
+      return key_temp
     
-    cube_side_length = 2 ** (total_cube_size // dimension)
-    
-    key_str = join_string_list(pad_or_truncate_key(key_hex))
-    key_str = pad_right_list(key_str, int((2**(total_cube_size))*dimension*(2**bit_shift_size)), "0")
+    key = pad_or_truncate_key(key_hex)
 
     arbitrary_key = []
     for cube_coordinate in range(2**total_cube_size):
         key_shifts = []
         for dim in range(dimension):
-            cube_offset = cube_coordinate*dimension*(2**bit_shift_size)
-            dimension_offset = dim*(2**bit_shift_size)
-
-            # Store shift amount as integer directly
-            shift_amount = int(key_str[cube_offset+dimension_offset:cube_offset+dimension_offset+(2**bit_shift_size)], 2)
+            key_index = cube_coordinate * dimension + dim
+            shift_amount = key[key_index] # Directly use byte value from key
             key_shifts.append(shift_amount)
         arbitrary_key.append(key_shifts)
 
@@ -76,34 +72,31 @@ def iterate_ndindex_backwards_generator(shape):
         index = np.unravel_index(i, shape)
         yield index
 
-def reverse_hypercube_and_reverse_shift(dimension, total_cube_size, key_hex, bit_shift_size, cube_data):
+def reverse_hypercube_and_reverse_shift(dimension, total_cube_size, key_hex, cube_data):  #Removed bit_shift_size
+    cube_side_length = 2 ** (total_cube_size // dimension)
+
     def pad_or_truncate_key(key_hex):
       key_temp = key_hex
-      size = int((2**(total_cube_size))*dimension*(2**bit_shift_size))
+      size = (2**total_cube_size) * dimension # Each shift is now 1 byte
       if len(key_hex) < size:
-        key_temp = pad_right_list(key_hex, size, "0")
+        key_temp = pad_right_list(key_hex, size, 0) #Pad with 0 instead of "0" (integers)
       elif len(key_hex) > size:
         key_temp = key_hex[0:size]
       else:
         key_temp = key_hex
-      return(key_temp)
-
-    cube_side_length = 2 ** (total_cube_size // dimension)
+      return key_temp
     
-    key_str_to_flip = join_string_list(pad_or_truncate_key(key_hex))
-    key_str_to_flip = pad_right_list(key_str_to_flip, int((2**(total_cube_size))*dimension*(2**bit_shift_size)), "0")
+    key = pad_or_truncate_key(key_hex) #No need to join string anymore
+
 
     arbitrary_key = []
     for cube_coordinate in range(2**total_cube_size):
         key_shifts = []
         for dim in range(dimension):
-            cube_offset = cube_coordinate*dimension*(2**bit_shift_size)
-            dimension_offset = dim*(2**bit_shift_size)
-
-            # Store shift amount as integer directly
-            bits_to_check = key_str_to_flip[cube_offset+dimension_offset:cube_offset+dimension_offset+(2**bit_shift_size)]
-            new_int_shift = (cube_side_length - int(bits_to_check, 2)) % cube_side_length
-            key_shifts.append(new_int_shift)
+            key_index = cube_coordinate * dimension + dim
+            shift_amount = (cube_side_length - key[key_index]) % cube_side_length  # Directly get byte value
+            key_shifts.append(shift_amount)
+        arbitrary_key.append(key_shifts)
 
     hypercube_shape = (cube_side_length,) * dimension
     hypercube = np.array(cube_data).reshape(hypercube_shape)
@@ -129,17 +122,13 @@ def reverse_hypercube_and_reverse_shift(dimension, total_cube_size, key_hex, bit
 
     return hypercube, shifted_hypercube
 
-def bytearray_to_bits_array(byte_data):
-    return [int(bit) for byte in byte_data for bit in format(byte, '08b')]
-
 def main():
     #power of 2 params
     byte_data = bytearray([random.randint(0, 255) for _ in range(256)]) # Random bytearray
 
     dimension = 3
-    total_cube_size = 15#2^3
-    bit_shift_size = 2 #2^2 shifting by 0 to 3
-    cube_data = bytearray_to_bits_array(byte_data) #total 2^3 = 8 entries 
+    total_cube_size = 18#2^3
+    cube_data = list(byte_data) #total 2^3 = 8 entries,  Keep as byte data
 
     # print(cube_data)
     if len(cube_data) < 2**(total_cube_size):
@@ -149,20 +138,22 @@ def main():
     if len(cube_data) > 2**(total_cube_size):
         cube_data = cube_data[0:2**(total_cube_size)]
 
-    key_byte = ["".join([str(random.randint(0,1)) for i in range(1)]) for i in range((2**(total_cube_size))*dimension*(2**bit_shift_size))]
+
+    #Generate a bytearray for key shifts with the length of (2**total_cube_size)*dimension (one byte per key)
+    key_byte = bytearray([random.randint(0, 255) for i in range((2**total_cube_size)*dimension)]) # Bytes
 
 
     hypercube, shifted_hypercube = create_hypercube_and_shift(
-        dimension, total_cube_size, key_byte, bit_shift_size, cube_data
+        dimension, total_cube_size, key_byte, cube_data  #Removed bit_shift_size
     )
 
     hypercube, og_hypercube = reverse_hypercube_and_reverse_shift(
-        dimension, total_cube_size, key_byte, bit_shift_size, shifted_hypercube
+        dimension, total_cube_size, key_byte, shifted_hypercube #Removed bit_shift_size
     )
 
 
     print("diff:")
-    print(hypercube-og_hypercube)
+    print(np.sum(np.abs(np.array(hypercube)-np.array(og_hypercube))))
 
 if __name__ == "__main__":
     # start = time.time()
