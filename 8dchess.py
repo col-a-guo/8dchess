89ce53f696ba96d0d401e6aaa34bf1e5a9532b55
Collin A Guo <bunjuku8@gmail.com>
2025-02-28 00:40:59 -0800

    bytes working, 40s for 2^18

--- 8dchess.py
+++ 8dchess.py
@@ -29,38 +29,24 @@ def create_hypercube_and_shift(dimension, total_cube_size, key_hex, cube_data): 
     
     key = pad_or_truncate_key(key_hex)
 
-    arbitrary_key = []
-    for cube_coordinate in range(2**total_cube_size):
-        key_shifts = []
-        for dim in range(dimension):
-            key_index = cube_coordinate * dimension + dim
-            shift_amount = key[key_index] # Directly use byte value from key
-            key_shifts.append(shift_amount)
-        arbitrary_key.append(key_shifts)
+    arbitrary_key = np.array(key).reshape((2**total_cube_size, dimension)) #Precompute key shifts as a numpy array
 
     hypercube_shape = (cube_side_length,) * dimension
-    hypercube = np.array(cube_data).reshape(hypercube_shape)
+    hypercube = np.array(cube_data, dtype=np.uint8).reshape(hypercube_shape) #Ensure uint8 dtype
 
     def rotate_bit_shift(hypercube, arbitrary_key, shift_dimension, line_index):
-        shifted_hypercube = hypercube.copy()
-        key_index = np.ravel_multi_index(tuple(line_index), hypercube.shape)
-        shift_amount = arbitrary_key[key_index][shift_dimension]
-
-        # Create a tuple of slices to select the line along the specified dimension
-        slices = [slice(None)] * hypercube.ndim
-        slices[shift_dimension] = line_index[shift_dimension] #Fixed: indexing the list of tuples here
-
-
-        shifted_hypercube = np.roll(shifted_hypercube, shift_amount, axis=shift_dimension)
-
+        # shift_amount = arbitrary_key[np.ravel_multi_index(tuple(line_index), hypercube.shape)][shift_dimension] # Moved this calculation outside the function for precompute advantage
+        shift_amount = arbitrary_key[np.ravel_multi_index(line_index, hypercube.shape)][shift_dimension]
+        shifted_hypercube = np.roll(hypercube, shift_amount, axis=shift_dimension) #No more copy()
         return shifted_hypercube
     
     shifted_hypercube = hypercube.copy()
 
+    #Correct the loop for all the shift_dimension
     for line_index in np.ndindex(*hypercube.shape):
+        for shift_dimension in range(dimension):
+            shifted_hypercube = rotate_bit_shift(shifted_hypercube, arbitrary_key, shift_dimension, line_index)
 
-      for shift_dimension in range(dimension):
-        shifted_hypercube = rotate_bit_shift(shifted_hypercube, arbitrary_key, shift_dimension, list(line_index))
 
     return hypercube, shifted_hypercube
 
@@ -87,48 +73,33 @@ def reverse_hypercube_and_reverse_shift(dimension, total_cube_size, key_hex, cub
       return key_temp
     
     key = pad_or_truncate_key(key_hex) #No need to join string anymore
+    arbitrary_key = np.array(key).reshape((2**total_cube_size, dimension)) #Precompute key shifts as a numpy array
 
-
-    arbitrary_key = []
-    for cube_coordinate in range(2**total_cube_size):
-        key_shifts = []
-        for dim in range(dimension):
-            key_index = cube_coordinate * dimension + dim
-            shift_amount = (cube_side_length - key[key_index]) % cube_side_length  # Directly get byte value
-            key_shifts.append(shift_amount)
-        arbitrary_key.append(key_shifts)
 
     hypercube_shape = (cube_side_length,) * dimension
-    hypercube = np.array(cube_data).reshape(hypercube_shape)
+    hypercube = np.array(cube_data, dtype=np.uint8).reshape(hypercube_shape) #Ensure uint8 dtype
 
     def rotate_bit_shift(hypercube, arbitrary_key, shift_dimension, line_index):
-        shifted_hypercube = hypercube.copy()
-        key_index = np.ravel_multi_index(tuple(line_index), hypercube.shape)
-        shift_amount = arbitrary_key[key_index][shift_dimension]
-
-        slices = [slice(None)] * dimension # make a list of ":" slices
-        slices[shift_dimension] = line_index[shift_dimension] #and then index the list here.
-
-        shifted_hypercube = np.roll(shifted_hypercube, shift_amount, axis=shift_dimension)
-
+        shift_amount = (cube_side_length - arbitrary_key[np.ravel_multi_index(line_index, hypercube.shape)][shift_dimension]) % cube_side_length #Precompute
+        shifted_hypercube = np.roll(hypercube, shift_amount, axis=shift_dimension) #No more copy()
         return shifted_hypercube
     
-    shifted_hypercube = hypercube.copy()
-    
+    shifted_hypercube = hypercube.copy() #Now cube has uint8 type
 
+    #Correct the loop for all the shift_dimension
     for line_index in iterate_ndindex_backwards_generator(hypercube.shape):
-      for shift_dimension in iterate_ndindex_backwards_generator(range(dimension)):
-        shifted_hypercube = rotate_bit_shift(shifted_hypercube, arbitrary_key, shift_dimension, list(line_index))
+        for shift_dimension in iterate_ndindex_backwards_generator(range(dimension)):
+            shifted_hypercube = rotate_bit_shift(shifted_hypercube, arbitrary_key, shift_dimension, line_index)
 
     return hypercube, shifted_hypercube
 
 def main():
     #power of 2 params
-    byte_data = bytearray([random.randint(0, 255) for _ in range(256)]) # Random bytearray
-
     dimension = 3
-    total_cube_size = 18#2^3
-    cube_data = list(byte_data) #total 2^3 = 8 entries,  Keep as byte data
+    total_cube_size = 18#
+
+    byte_data = bytearray([random.randint(0, 255) for _ in range(2**total_cube_size)]) # Random bytearray
+    cube_data = list(byte_data) #
 
     # print(cube_data)
     if len(cube_data) < 2**(total_cube_size):
